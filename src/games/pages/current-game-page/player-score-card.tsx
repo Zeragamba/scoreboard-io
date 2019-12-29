4ed75a3a8b17f60bdb953dd29db659ec476fe3dd
Stephen A. Wilson <Stephen-356@hotmail.com>
2019-12-29 16:30:52 -0500

    Add score changing

--- src/games/pages/current-game-page/player-score-card.tsx
+++ src/games/pages/current-game-page/player-score-card.tsx
@@ -1,7 +1,9 @@
 import {Button, Card, CardActions, CardContent} from '@material-ui/core';
 import React from 'react';
+import {useDispatch} from 'react-redux';
 
 import Player from '../../../players/player';
+import {ChangeScore} from '../../../players/player.actions';
 
 import './player-score-card.scss';
 
@@ -10,17 +12,20 @@ interface PlayerScoreCardProps {
 }
 
 const PlayerScoreCard:React.FC<PlayerScoreCardProps> = ({player}) => {
+  const dispatch = useDispatch();
+  const onScoreChange = (amount:number) => dispatch(ChangeScore(player, amount));
+
   return (
     <Card className={'player-score-card'}>
       <CardContent>
         {player.name}
       </CardContent>
       <CardActions>
-        <Button variant={'outlined'}>-10</Button>
-        <Button variant={'outlined'}>-1</Button>
-        <span className={'cur-score'}>0</span>
-        <Button variant={'outlined'}>+1</Button>
-        <Button variant={'outlined'}>+10</Button>
+        <Button variant={'outlined'} onClick={() => onScoreChange(-10)}>-10</Button>
+        <Button variant={'outlined'} onClick={() => onScoreChange(-1)}>-1</Button>
+        <span className={'cur-score'}>{player.score}</span>
+        <Button variant={'outlined'} onClick={() => onScoreChange(+1)}>+1</Button>
+        <Button variant={'outlined'} onClick={() => onScoreChange(+10)}>+10</Button>
       </CardActions>
     </Card>
   );
